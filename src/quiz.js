2e16940eb19e8e2c4a8139f0e0071ad9817fdf1b
Krists Krievins <krists.krievins@gmail.com>
2025-02-25 17:20:02 +0100

    Day 2 Solved

--- src/quiz.js
+++ src/quiz.js
@@ -1,37 +1,57 @@
 class Quiz {
-    constructor (questions, timeLimit, timeRemaining) {
-        this.questions = questions; // array
-        this.timeLimit = timeLimit; // number
-        this.timeRemaining = timeRemaining; // number
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    }
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions; // array
+    this.timeLimit = timeLimit; // number
+    this.timeRemaining = timeRemaining; // number
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    getQuestion() {
-        return this.questions [this.currentQuestionIndex]; 
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    }
-    
-    moveToNextQuestion() {
-        this.currentQuestionIndex += 1;
-    }
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
 
-    shuffleQuestions() {
-            const shuffledQuestions = this.questions.sort((a, b) => 0.5 - Math.random());
-            return shuffledQuestions;
-    }
-    checkAnswer(answer) {
-        this.correctAnswers +=1;
-    }
+  shuffleQuestions() {
+    const shuffledQuestions = this.questions.sort(
+      (a, b) => 0.5 - Math.random()
+    );
+    return shuffledQuestions;
+  }
+  checkAnswer(answer) {
+    this.correctAnswers += 1;
+  }
 
-    hasEnded() {
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-        if (this.currentQuestionIndex < this.questions.length) {
-            return false;
+  filterQuestionsByDifficulty(difficulty) {
+    // difficulty between 1 - 3
+    if (difficulty > 0 && difficulty <= 3) {
+      const filteredQuestions = this.questions.filter((oneQuestion) => {
+        if (oneQuestion.difficulty === difficulty) {
+          return true;
+        } else {
+          return false;
         }
-        else {
-            return true;
-        }
+      });
+      this.questions = filteredQuestions;
+    } else {
+      return false;
     }
-
+  }
+  averageDifficulty() {
+    const difficultySum = this.questions.reduce((acc, current) => {
+      return acc + current.difficulty;
+    }, 0);
+    return difficultySum / this.questions.length;
+  }
 }
